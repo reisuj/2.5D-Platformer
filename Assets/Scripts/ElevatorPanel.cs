a5b603dfe2ccbf2fe05ce599fa887921fea99ba4
Brian Branch <reisuj@gmail.com>
2021-08-30 07:59:48 -0400

    Elevator panel button functionality added

---
+++ Assets/Scripts/ElevatorPanel.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorPanel : MonoBehaviour
+{
+    [SerializeField]
+    private MeshRenderer _callButton;
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                _callButton.material.color = Color.green;
+                // Call the elevator
+            }
+        }
+    }
+}
